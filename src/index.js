1982b832a0a400cd68b7195b6cbf221e6aa7e70d
Greg Kurman <60169807+Greg-Kurman@users.noreply.github.com>
2020-05-13 15:23:56 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,37 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import State, {subscribe} from './components/redux/state';
+import {addPost , updateNewPostText} from './components/redux/state';
+import { BrowserRouter } from 'react-router-dom';
 
 
-let messageData = [
-  {id:1, message: "why"},
-  {id:2, message: "Hello"},
-  {id:3, message: "hiy"},
 
-];
-
-let dialogsData = [
-  {id:1, name: "Dimach"},
-  {id:2, name: "Greg"},
-  {id:3, name: 'anton'},
-  {id:4, name: 'Gordon'},
-  {id:5, name: 'sasha'},
-  {id:6, name: 'lolka'},
-];
-
-let postData = [
-  {id:1 ,  message:"hi hello", likecount:12},
-  {id:2, message:"hi hello", likecount:2},
-  {id:3, message:"hi hello", likecount:1},
-];
+let rerenderEntireTree = () => {
+  
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App postData={postData} dialogsData={dialogsData} messageData={messageData}/>
-  </React.StrictMode>,
+
+    <BrowserRouter>
+    <App state={State} addPost={addPost} updateNewPostText={updateNewPostText}/>,
+    </BrowserRouter>,
+
   document.getElementById('root')
-);
+)
+
+};
+
+rerenderEntireTree();
+
+subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
